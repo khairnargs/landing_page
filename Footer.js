55def662d7682f57b76dd262db46875127bbb53b
khairnargs <125984819+khairnargs@users.noreply.github.com>
2023-09-08 19:32:37 +0530

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <section class="footer">     
+        
+<h1 class="credit">  <small>Copyright © 2023 - All Rights Reserved by SIMCA. </small>
+        <small>Version : v1.1.1 </small> Designed with <i class="fa fa-heart pulse"></i> by  Prasad, Gayatri.</h1>
+
+</section>
+
+    </>
+  )
+}
+
+export default Footer
