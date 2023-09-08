45fa60f59a2a2ae00e0ad03c7d84c1a7d88632e3
khairnargs <125984819+khairnargs@users.noreply.github.com>
2023-09-08 19:28:14 +0530

    Add files via upload

---
+++ LandingPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './LandingPage.css';
+
+const LandingPage = () => {
+  return (
+    <div className="landing-page">
+      <h1>Hello, Welcome to SIMCA</h1>
+      <br/>
+      <hr />
+     
+    </div>
+  );
+};
+
+export default LandingPage;
